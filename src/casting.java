0faec8f2c0e1e92348b94446bb1960e8620e6700
ongvang00 <you@example.com>
2023-05-09 09:01:32 -0400

    sfa

---
+++ src/casting.java
@@ -0,0 +1,19 @@
+public class casting {
+    public static void main(String[] args) {
+        // create double type variable
+        double num = 112.35;
+        System.out.println("The double value: " + num);
+
+        // convert into int type
+        int data = (int)num;
+        System.out.println("The integer value: " + data);
+
+        // create string type variable
+        String fortynine = "49";
+        System.out.println("The string value is: " + fortynine);
+
+        // convert string variable to int
+        int number = Integer.parseInt(fortynine);
+        System.out.println("The integer value is: " + number);
+    }
+}
